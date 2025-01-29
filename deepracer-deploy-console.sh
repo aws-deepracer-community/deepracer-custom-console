bcd950c531dceb8d1cb0642d52384f3bb9365561
Mark Ross <mark.ross@atos.net>
2025-01-29 09:00:05 +0000

    update bash scripts

--- deepracer-deploy-console.sh
+++ deepracer-deploy-console.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#sudo cp /opt/aws/deepracer/lib/device_console /opt/aws/deepracer/lib/device_console_old -r
 unzip deepracer-console-new.zip -d temp_website
 sudo cp -a temp_website/static/. /opt/aws/deepracer/lib/device_console/static/ -r
 sudo cp temp_website/index.html /opt/aws/deepracer/lib/device_console/templates -f
