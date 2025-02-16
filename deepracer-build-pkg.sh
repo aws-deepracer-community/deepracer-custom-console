62aa455fff47d2d44551031208c3b9d3c634c611
Lars Ludvigsen <larsll@outlook.com>
2025-02-16 16:16:44 +0100

    Fix manifest location

--- deepracer-build-pkg.sh
+++ deepracer-build-pkg.sh
@@ -9,7 +9,7 @@ cp $DIR/package/prerm dist/DEBIAN/
 
 cp -r $DIR/new/dist/* $DIR/dist/opt/aws/deepracer/lib/device_console
 mv $DIR/dist/opt/aws/deepracer/lib/device_console/*.html $DIR/dist/opt/aws/deepracer/lib/device_console/templates
-mv $DIR/dist/opt/aws/deepracer/lib/device_console/*.json $DIR/dist/opt/aws/deepracer/lib/device_console/templates
+mv $DIR/dist/opt/aws/deepracer/lib/device_console/*.json $DIR/dist/opt/aws/deepracer/lib/device_console/static
 
 dpkg-deb --root-owner-group --build $DIR/dist $DIR/dist/aws-deepracer-device-console.deb
 dpkg-name -o $DIR/dist/aws-deepracer-device-console.deb
